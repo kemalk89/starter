a64d30f1e91f9a72314cd563ab9ab9add5557d14
kemal <k.kemal.1989@gmail.com>
2021-03-17 07:01:27 +0100

    Integration of keycloak.

--- README.md
+++ README.md
@@ -9,9 +9,11 @@ connected with a database.
 Simply run the following commands to get started:
 0. Install and start docker
 1. ```cd docker```
-2. ```docker-compose up app_database``` -> This will start all the infrastructure
+2. ```docker-compose up app_db iam``` > This will start all the infrastructure
 3. Run the application
+4. Open Postman and 
 4. Open browser and call http://localhost:8081/hello
+
 
 # Your TODOs
 - Currently project name is demo. You should change this to your projects name
--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,16 +1,46 @@
 package com.example.demo.security;
 
-import org.springframework.context.annotation.Configuration;
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@KeycloakConfiguration
+@ConditionalOnProperty(name = "keycloak.enabled", havingValue = "true")
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new NullAuthenticatedSessionStrategy();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    /**
+     * By Default, the Spring Security Adapter looks for a keycloak.json configuration file.
+     * You can make sure it looks at the configuration provided by the Spring Boot Adapter
+     * by adding this bean.
+     */
+    @Bean
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+
         http
                 .authorizeRequests()
                 .antMatchers("/api/*").hasRole("USER")
